88dd1b8b6c91806fade95762f9cedfce5113a489
canonal <onalcan20@gmail.com>
2022-01-21 17:47:05 +0300

    refactored LargeNews Model

--- app/src/main/java/com/canonal/news/model/LargeNews.kt
+++ app/src/main/java/com/canonal/news/model/LargeNews.kt
@@ -3,7 +3,7 @@ package com.canonal.news.model
 import android.graphics.drawable.Drawable
 
 data class LargeNews(
-    val title: String,
+    val  title: String,
     val image: Drawable?
-)
+): News(title, image)
 
