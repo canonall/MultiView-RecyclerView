321e68385ecfa817d8feadbc3765907608680462
canonal <onalcan20@gmail.com>
2022-01-21 17:45:09 +0300

    migrated NewsAdapter to multiple views

--- app/src/main/java/com/canonal/news/adapter/NewsAdapter.kt
+++ app/src/main/java/com/canonal/news/adapter/NewsAdapter.kt
@@ -4,25 +4,65 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.canonal.news.databinding.ItemLargeNewsBinding
+import com.canonal.news.databinding.ItemShortNewsBinding
 import com.canonal.news.model.LargeNews
+import com.canonal.news.model.News
+import com.canonal.news.model.NewsType
+import com.canonal.news.model.ShortNews
 
 class NewsAdapter(
-    private val largeNewsList: List<LargeNews>
-) : RecyclerView.Adapter<LargeNewsViewHolder>() {
+    private val newsList: List<News>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    private lateinit var binding: ItemLargeNewsBinding
+    private lateinit var bindingLargeNews: ItemLargeNewsBinding
+    private lateinit var bindingShortNews: ItemShortNewsBinding
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LargeNewsViewHolder {
-        binding = ItemLargeNewsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return LargeNewsViewHolder(binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        when (viewType) {
+            NewsType.SHORT_NEWS.value -> {
+                bindingShortNews = ItemShortNewsBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+                ShortNewsViewHolder(bindingShortNews)
+            }
+            NewsType.LARGE_NEWS.value -> {
+                bindingLargeNews = ItemLargeNewsBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+                LargeNewsViewHolder(bindingLargeNews)
+            }
+            else -> {
+                bindingLargeNews = ItemLargeNewsBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+                LargeNewsViewHolder(bindingLargeNews)
+            }
+        }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(newsList[position]){
+            is ShortNews -> {
+                (holder as ShortNewsViewHolder).bind(newsList[position] as ShortNews)
+            }
+            is LargeNews -> {
+                (holder as LargeNewsViewHolder).bind(newsList[position] as LargeNews)
+            }
+        }
     }
 
-    override fun onBindViewHolder(holder: LargeNewsViewHolder, position: Int) {
-        val largeNews = largeNewsList[position]
-        holder.bind(largeNews)
-    }
+    override fun getItemCount(): Int = newsList.size
 
-    override fun getItemCount(): Int = largeNewsList.size
-
+    override fun getItemViewType(position: Int): Int =
+        when (newsList[position]) {
+            is ShortNews -> {
+                NewsType.SHORT_NEWS.value
+            }
+            is LargeNews -> {
+                NewsType.LARGE_NEWS.value
+            }
+            else -> {
+                NewsType.LARGE_NEWS.value
+            }
+        }
 }
 
